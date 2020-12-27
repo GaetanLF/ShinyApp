28965ad482143170cc2c522fa05d7973305527bb
Gaëtan LF <61993231+GaetanLF@users.noreply.github.com>
2020-12-27 22:38:29 +0100

    Add files via upload

--- app.R
+++ app.R
@@ -66,6 +66,9 @@ tabsetPanel(
              gaetan.le-floch[at]etu.univ-paris1.fr. Veuillez noter que certaines figures peuvent mettre du temps pour apparaître si vous
              utilisez le fichier brut disponible sur Github. Vous ne devriez pas avoir ce problème si vous accédez à l'application par
              shinyapps.io."),
+           p(div(HTML("En cas de besoin, vous pouvez accéder au <a href='https://github.com/GaetanLF/ShinyApp' target='_blank'
+                      >répertoire Github</a> ou au <a href='#' target='_blank'>rapport édité dans le cadre du contrôle</a>, qui
+                      contient des informations non disponibles sur cette application."))),
            p("Nous vous souhaitons une bonne exploration !")),
   
   tabPanel("Exercice 1",
@@ -73,22 +76,28 @@ tabsetPanel(
   
   tabPanel("Exercice 2",
            titlePanel("Exercice 2 : pluviométrie dans les villes françaises"),
-           h2("Partie I : l'analyse en composantes principales"),
-           wellPanel(
-             checkboxGroupInput(inputId="QuantiSupp",
-                                label="Indiquez les variables supplémentaires",
-                                choices=colnames(dataVilles),
-                                selected=colnames(dataVilles)[13:16],
-                                inline=T),
-             radioButtons(inputId="Habillage",
-                          label="Pour le graphique des individus statistiques, veuillez choisir le mois d'intérêt.",
-                          choices = colnames(dataVilles)[1:12],
-                          selected = colnames(dataVilles)[1:12][1],
-                          inline=T)),
+           
+           
            
            mainPanel(align="center",fluidRow(
+             
+             column(12,h2("Partie I : l'analyse en composantes principales")),
+             
+             column(12,wellPanel(
+               checkboxGroupInput(inputId="QuantiSupp",
+                                  label="Indiquez les variables supplémentaires",
+                                  choices=colnames(dataVilles),
+                                  selected=colnames(dataVilles)[13:16],
+                                  inline=T),
+               radioButtons(inputId="Habillage",
+                            label="Pour le graphique des individus statistiques, veuillez choisir le mois d'intérêt.",
+                            choices = colnames(dataVilles)[1:12],
+                            selected = colnames(dataVilles)[1:12][1],
+                            inline=T))),
+             
              column(6,plotOutput(outputId="PCAVar")),
              column(6,plotOutput(outputId = "PCAind")),
+             
              column(12,h3("Question 1")),
              column(12,p("Ici, le plan factoriel explique 98.81% de l'inertie. Ainsi, les deux dimensions sont largement suffisantes pour une
              analyse cohérente vu qu'elle contiennent la quasi-totalité de l'information.")),
@@ -115,76 +124,85 @@ tabsetPanel(
                          On conclut que Lille et Strasbourg s’opposent bien totalement à Montpellier et Marseille qui restent toujours chaudes tout au long de l’année (relativement aux autres villes).")),
              column(12,h3("Question 5")),
              column(12,p("Brest, Rennes et Nantes sont assez similaire comme vu dans les graphiques précédents : leurs températures sont très variables avec la saison (été ou hiver) : Il fera, relativement aux autres villes,  chaud en hiver et froid en été.
-             On peut les opposer à Vichy Clermont, paris et Grenoble où il fera relativement plus chaud en été qu’en hiver."))
-           )),
-           
-           
-           h2("Partie II : le clustering"),
-           
-           wellPanel(sliderInput(inputId="NClust",
-             label="Choisissez le nombre de classes à prendre en compte dans l'algorithme des k-means : ",
-             min=2,
-             max=6,
-             value=2
-           ),
-           checkboxGroupInput(inputId="VarCube",label="Pour le graphique 3D, veuillez choisir les variables que vous souhaitez
-                              représenter : ")),
-
-           mainPanel(align="right",fluidRow(
+             On peut les opposer à Vichy Clermont, paris et Grenoble où il fera relativement plus chaud en été qu’en hiver.")),
+             
+             
+             
+             column(12,h2("Partie II : le clustering")),
+             column(12,wellPanel(sliderInput(inputId="NClust",
+                                             label="Choisissez le nombre de classes à prendre en compte dans l'algorithme des k-means : ",
+                                             min=2,
+                                             max=6,
+                                             value=3
+             ))),
+             
              column(6,plotOutput(outputId="HCPlot"),),
-             column(6,tableOutput(outputId = "KTab"))
-           )),
+             column(6,plotOutput(outputId = "KPlot")),
+             column(12,h3("Question 1")),
+             column(12,p("Il nous paraît naturel d’utiliser trois clusters pour l'algorithme des k-means. 
+                         Cette classification correspond exactement à celle proposée par le clustering hiérarchique et elle demeure
+                         cohérente. De plus, 3 classes semble être un bon compromis entre le nombre de clusters et la maximisation de la part de la 
+                         variance inter-classes dans la variance totale.")),
+             column(12,h3("Question 2")),
+             column(12,p("Le dendogramme semble confirmer notre intuition avec les k-means : nous avons un cluster pour les villes du Sud, un cluster pour les villes du 
+                         littoral breton et un dernier pour les villes soumises à un climat plus continental."))
+             
+           )), # Close fluidrow and Mainpanel from exercise 2
   
-        ),
+        ), # Close Tabpanel from exercise 2
 
   tabPanel("Exercice 3",
             titlePanel("Exercice 3 : Analyse des filières universitaires"),
-            wellPanel(
-                  checkboxGroupInput(inputId="Supp",
-                                     label="Indiquez les variables supplémentaires",
-                                     choices=colnames(dataUniv),
-                                     selected=colnames(dataUniv)[7:12],
-                                     inline=T,
-                                     choiceNames = colnames(dataUniv), # Asks the user for supplementary variables
-                                     choiceValues = 1:ncol(dataUniv))), # End of wellPanel
-                  
-                  
-            plotOutput(outputId = "ACGraph"), # Plots the 2D corresponding analysis.
-            
-            
-            tableOutput(outputId = "cos21"), # Shows cos2 from the 2D factorial plan
-            tableOutput(outputId = "cos22"),
-            h3("Question 1"),
-            p("Ici, le plan factoriel explique 86.23% de l'inertie. Ainsi, les deux dimensions du plan contiennent la grande majorité
-              de l'information."),
-            h3("Question 2"),
-            p("Les études de lettres et de langues sont bien représentées, de même que les femmes en licence. Pour les masters et les doctorats,
-                    c'est un peu plus compliqué. Globalement, les points représentant les femmes et les études de langues sont assez proches.
-                    Nous pouvons raisonnablement penser que les femmes sont plus attirées par les études de lettres, au moins en licence."),
-            h3("Question 3"),
-            p("Au total, les hommes sont très bien représentés sur le plan factoriel. Il en est de même pour chaque niveau de diplôme où les cos2
-                    ne sont jamais en-dessous de 60%. Au niveau des sciences, seules les sciences humaines et les sciences fondamentales sont bien
-                    représentées ; les sciences économiques le sont aussi un peu dans une certaine mesuree, mais la SVT est mal représentée."),
-            p("Sur la comparaison Hommes/Sciences, ils sont très proches des sciences fondamentales et, dans une certaine mesure, des sciences
-                    économiques (même si rappelons-le les sciences éco ne sont pas très bien représentées). Nous pouvons affirmer que les hommes sont
-                    attirés, surtout au niveau master, par les sciences fondamentales. Par ailleurs, les hommes sont très éloignés de STAPS, mais tout
-                    de même plus proches que les femmes."),
-            h3("Question 4"),
-            p("Le plan factoriel représente extrêmement bien les études d'AES (les cos2 sont presque égaux à 1 en les additionnant). Les licences
-                    sont assez proches d'AES et extrêmement bien représentées aussi. Les masters dans leur ensemble sont bien représentés, tout comme
-                    les doctorats, et ils sont tous les deux très éloignés de AES. Nous pouvons affirmer que ces études sont plutôt courtes."),
-            h3("Question 5"),
-            p("Le premier axe, de la dimension 1, pourrait séparer les études littéraires des études scientifiques : le point le plus à gauche
-                    représente les études de langues et le point le plus à droite représente les sciences fondamentales. Le deuxième axe, lui,
-                    pourrait séparer les études plutôt longues de celles plutôt courtes, comme en témoigne essentiellement le placement des points
-                    Licence, Master et Doctorat sur le plan factoriel."),
-            h3("Question 6"),
-            p("On ne peut pas penser grand chose de la proximité entre ces deux points : les femmes en master sont mal représentées (~0.47)
-                    et les études de SVT également (~0.42) : ces points ne sont pas interprétables et nous ne pouvons conclure à une supposée 
-                    proximité."),
-            h3("Question 7"),
-            p("Lettres et arts sont extrêmement bien représentées (~0.91) et les femmes en licence également (~0.97). Il y a une très forte
-                    proximité entre ces deux catégories.")
+           
+            mainPanel(align="center",fluidRow(
+                        column(12,wellPanel(
+                              checkboxGroupInput(inputId="Supp",
+                                                 label="Indiquez les variables supplémentaires",
+                                                 choices=colnames(dataUniv),
+                                                 selected=colnames(dataUniv)[7:12],
+                                                 inline=T,
+                                                 choiceNames = colnames(dataUniv), # Asks the user for supplementary variables
+                                                 choiceValues = 1:ncol(dataUniv)))), # End of wellPanel
+                              
+                              
+                        column(12,plotOutput(outputId = "ACGraph")), # Plots the 2D corresponding analysis.
+                        
+                        
+                        column(12,tableOutput(outputId = "cos21")), # Shows cos2 from the 2D factorial plan
+                        column(12,tableOutput(outputId = "cos22")),
+                        column(12,h3("Question 1")),
+                        column(12,p("Ici, le plan factoriel explique 86.23% de l'inertie. Ainsi, les deux dimensions du plan contiennent la grande majorité
+                          de l'information.")),
+                        column(12,h3("Question 2")),
+                              column(12,p("Les études de lettres et de langues sont bien représentées, de même que les femmes en licence. Pour les masters et les doctorats,
+                                c'est un peu plus compliqué. Globalement, les points représentant les femmes et les études de langues sont assez proches.
+                                Nous pouvons raisonnablement penser que les femmes sont plus attirées par les études de lettres, au moins en licence.")),
+                        column(12,h3("Question 3")),
+                        column(12,p("Au total, les hommes sont très bien représentés sur le plan factoriel. Il en est de même pour chaque niveau de diplôme où les cos2
+                                ne sont jamais en-dessous de 60%. Au niveau des sciences, seules les sciences humaines et les sciences fondamentales sont bien
+                                représentées ; les sciences économiques le sont aussi un peu dans une certaine mesuree, mais la SVT est mal représentée.")),
+                        column(12,p("Sur la comparaison Hommes/Sciences, ils sont très proches des sciences fondamentales et, dans une certaine mesure, des sciences
+                                économiques (même si rappelons-le les sciences éco ne sont pas très bien représentées). Nous pouvons affirmer que les hommes sont
+                                attirés, surtout au niveau master, par les sciences fondamentales. Par ailleurs, les hommes sont très éloignés de STAPS, mais tout
+                                de même plus proches que les femmes.")),
+                        column(12,h3("Question 4")),
+                        column(12,p("Le plan factoriel représente extrêmement bien les études d'AES (les cos2 sont presque égaux à 1 en les additionnant). Les licences
+                                sont assez proches d'AES et extrêmement bien représentées aussi. Les masters dans leur ensemble sont bien représentés, tout comme
+                                les doctorats, et ils sont tous les deux très éloignés de AES. Nous pouvons affirmer que ces études sont plutôt courtes.")),
+                        column(12,h3("Question 5")),
+                        column(12,p("Le premier axe, de la dimension 1, pourrait séparer les études littéraires des études scientifiques : le point le plus à gauche
+                                représente les études de langues et le point le plus à droite représente les sciences fondamentales. Le deuxième axe, lui,
+                                pourrait séparer les études plutôt longues de celles plutôt courtes, comme en témoigne essentiellement le placement des points
+                                Licence, Master et Doctorat sur le plan factoriel.")),
+                        column(12,h3("Question 6")),
+                        column(12,p("On ne peut pas penser grand chose de la proximité entre ces deux points : les femmes en master sont mal représentées (~0.47)
+                                et les études de SVT également (~0.42) : ces points ne sont pas interprétables et nous ne pouvons conclure à une supposée 
+                                proximité.")),
+                        column(12,h3("Question 7")),
+                        column(12,p("Lettres et arts sont extrêmement bien représentées (~0.91) et les femmes en licence également (~0.97). Il y a une très forte
+                                proximité entre ces deux catégories."))
+                        
+            )) # Close mainPanel from exercise 3
   
            
   ) # End of exercise 3
@@ -255,8 +273,14 @@ server <- function(input,output) {
     plot(hc)
   })
   
-  output$KTab <- renderPlot({
+  output$KPlot <- renderPlot({
     K <- Ex2KM()
+    dataVilles2 = dataVilles
+    dataVilles2$Clust = K$cluster
+    colorsClust <- c("green","blue","red","black","purple","yellow")
+    plot(x=dataVilles$longi,y=dataVilles$lati,main="Représentation des villes par position géographique et par 
+         cluster",xlab="Longitude",ylab="Latitude",col=colorsClust[dataVilles2$Clust])
+    text(dataVilles$longi, dataVilles$lati, labels=rownames(dataVilles), cex= 0.7, pos=2)
   })
   
 
